dd038c1a65f19bdc138604d4d6933cbcd435b396
Mohammad Adnan <mohammadadnan6460@gmail,com>
2024-01-10 22:49:30 +0530

    this is comment

---
+++ Heading.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+ function Heading(){
+    <h1>This is my heading</h1>
+ }
+ export default Heading;
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { ReactDOM } from "react";
+import Heading from "./Heading";
+
+ReactDOM.render(
+    <Heading />,
+    document.getElementsByClassName("root")
+);
+
+// document.getElementById("boot").innerHTML = "Hello World";
